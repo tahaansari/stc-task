38680dfd547fcb4244d624d6e1c6ecedb78562f1
Taha Ansari <taha.m.ansari@gmail.com>
2023-12-15 04:21:12 +0530

    Updates

---
+++ precache-manifest.4437ac9f29f10d3858bb55b89f4fea82.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b1a334cb98335cc4dba34990a4ed60e",
+    "url": "/stc-task/index.html"
+  },
+  {
+    "revision": "8e29ab1037073c480c5d",
+    "url": "/stc-task/static/css/2.0a77fee7.chunk.css"
+  },
+  {
+    "revision": "f12537ae5b743f306a23",
+    "url": "/stc-task/static/css/main.64a68ebb.chunk.css"
+  },
+  {
+    "revision": "8e29ab1037073c480c5d",
+    "url": "/stc-task/static/js/2.096d1d11.chunk.js"
+  },
+  {
+    "revision": "f12537ae5b743f306a23",
+    "url": "/stc-task/static/js/main.adb8125e.chunk.js"
+  },
+  {
+    "revision": "5e72a92e982edec525d5",
+    "url": "/stc-task/static/js/runtime-main.541a3b77.js"
+  },
+  {
+    "revision": "4e7c12c0eea7230afc5b2f26e333e0f0",
+    "url": "/stc-task/static/media/STCForward-Bold.4e7c12c0.woff2"
+  },
+  {
+    "revision": "d5b9e5c264be6653f1c2570a0ccf898c",
+    "url": "/stc-task/static/media/STCForward-Bold.d5b9e5c2.woff"
+  },
+  {
+    "revision": "8e83d9b23b20d74f5cca5e4d5c7fadb2",
+    "url": "/stc-task/static/media/STCForward-Medium.8e83d9b2.woff2"
+  },
+  {
+    "revision": "d011a120a27e48d37c3f6e2f0390c817",
+    "url": "/stc-task/static/media/STCForward-Medium.d011a120.woff"
+  },
+  {
+    "revision": "913a05d1b17d45585593f93ba335a34a",
+    "url": "/stc-task/static/media/STCForward-Regular.913a05d1.woff"
+  },
+  {
+    "revision": "9ddd04512ca983877dbf5a1c2f262f46",
+    "url": "/stc-task/static/media/STCForward-Regular.9ddd0451.woff2"
+  },
+  {
+    "revision": "0c591918adc0f8ecb418b3f09cd568e2",
+    "url": "/stc-task/static/media/STCForward-Thin.0c591918.woff2"
+  },
+  {
+    "revision": "a0fb4051cffd0deabfd9216e1faa1099",
+    "url": "/stc-task/static/media/STCForward-Thin.a0fb4051.woff"
+  },
+  {
+    "revision": "3646a39b6ef5417f27519bada8bbf786",
+    "url": "/stc-task/static/media/bootstrap-icons.3646a39b.woff2"
+  },
+  {
+    "revision": "abec3e4d4685334f4c88478830b0afcb",
+    "url": "/stc-task/static/media/bootstrap-icons.abec3e4d.woff"
+  },
+  {
+    "revision": "e364ffe827e0cd6a6c984cba21622e2d",
+    "url": "/stc-task/static/media/logo-white.e364ffe8.svg"
+  },
+  {
+    "revision": "04404d990fe8cbb15c8a30c3f2b4c792",
+    "url": "/stc-task/static/media/logo.04404d99.svg"
+  },
+  {
+    "revision": "7af2bfae0fe9c1f5cb902d8703378779",
+    "url": "/stc-task/static/media/react-task.7af2bfae.png"
+  },
+  {
+    "revision": "93f8a44a55fe981c4bde245ef38bc478",
+    "url": "/stc-task/static/media/tag.93f8a44a.svg"
+  }
+]);
